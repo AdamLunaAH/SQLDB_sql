c593d9a5312543e3ceb16c6bdd0fa3444ad9d381
Adam <161696943+AdamLunaAH@users.noreply.github.com>
2025-05-27 11:02:25 +0200

    Update

--- sql-scripts/Lesson12/L1201-create-view.sql
+++ sql-scripts/Lesson12/L1201-create-view.sql
@@ -8,10 +8,10 @@ GO
 CREATE VIEW dbo.vw_customer AS
 --ALTER VIEW dbo.vw_customer AS
     SELECT c.customer_id [cus id], c.last_name [cus last name], c.first_name [cus first name],
-        ci.city[cus city], cty.country [cus country], 
+        ci.city[cus city], cty.country [cus country],
         c.store_id [sto id]
     FROM dbo.customer AS c
-    
+
     --work out inner joins for customer -> address -city - >country
     INNER JOIN dbo.address AS a ON c.address_id = a.address_id
     INNER JOIN dbo.city AS ci ON a.city_id = ci.city_id
@@ -22,7 +22,7 @@ CREATE VIEW dbo.vw_store AS
 --ALTER VIEW dbo.vw_store AS
     SELECT s.store_id [store id], ci.city [store city], cty.country [store country]
     FROM dbo.store AS s
-    
+
     --work out inner joins for store -> address -> city -> country
     INNER JOIN dbo.address AS a ON s.address_id = a.address_id
     INNER JOIN dbo.city AS ci ON a.city_id = ci.city_id
@@ -33,4 +33,4 @@ SELECT TOP 5 * FROM dbo.vw_customer;
 SELECT * FROM dbo.vw_store;
 GO
 
-    
+
--- sql-scripts/Lesson13/L1302-try-catch.sql
+++ sql-scripts/Lesson13/L1302-try-catch.sql
@@ -2,7 +2,7 @@ USE friends;
 GO
 
 --Let's first create an application log table
-DROP TABLE IF EXISTS dbo.ErrorLog; --Housecleaning for the example     
+DROP TABLE IF EXISTS dbo.ErrorLog; --Housecleaning for the example
 CREATE TABLE dbo.ErrorLog(
     err_nr INT,
     err_msg NVARCHAR(400),
@@ -15,13 +15,13 @@ GO
 
 BEGIN TRY
     INSERT INTO dbo.Pet
-        (PetId, AnimalKind, Name, FriendId) VALUES 
+        (PetId, AnimalKind, Name, FriendId) VALUES
         (5, 'Cat', 'Whiskey', 1);
 END TRY
 BEGIN CATCH
-    --You can put some corrective in place, such as your application logging   
-    INSERT INTO dbo.ErrorLog (err_nr, err_msg, err_line, err_sev)
-    VALUES (ERROR_NUMBER(), ERROR_MESSAGE(), ERROR_LINE(), ERROR_SEVERITY())
+    --You can put some corrective in place, such as your application logging
+        INSERT INTO dbo.ErrorLog (err_nr, err_msg, err_line, err_sev)
+        VALUES (ERROR_NUMBER(), ERROR_MESSAGE(), ERROR_LINE(), ERROR_SEVERITY())
 
     --And then THROW the same error again to a client
     --;THROW
